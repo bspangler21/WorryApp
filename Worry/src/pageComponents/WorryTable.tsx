3e500177a9282f854c015a05bf9241423c3fe229
bspang <bspangler21@alumni.uwosh.edu>
2024-04-04 18:05:10 -0500

    Update WorryTable component and Worries component

--- Worry/src/pageComponents/WorryTable.tsx
+++ Worry/src/pageComponents/WorryTable.tsx
@@ -1,5 +1,6 @@
-import { mergeStyleSets } from "@fluentui/react";
+import { FontIcon, mergeStyleSets } from "@fluentui/react";
 import { Worry } from "../types/Worry";
+import { TableHeader } from "../types/TableHeader";
 
 const classNames = mergeStyleSets({
 	centeredTableHeader: {
@@ -10,14 +11,75 @@ const classNames = mergeStyleSets({
 		height: "75px",
 		cursor: "pointer",
 	},
+	smallTableHeader: {
+		textAlign: "center",
+		width: "300px",
+	},
 });
 
-export const WorryTable = (data: Worry[], headers: string[]) => {
+type Args = {
+	data: Worry[];
+	headers: TableHeader[];
+};
+
+export const WorryTable = ({ data, headers }: Args) => {
 	return (
 		<>
 			<div className={classNames.centeredTableHeader}>
 				<table>
-					<thead><tr>{headers.map((h) => <th></th>)} </tr></thead>
+					<thead>
+						<tr>
+							{headers.map((h) => (
+								<th>{h.name}</th>
+							))}
+						</tr>
+					</thead>
+					<tbody>
+						{data.map((d) => (
+							<>
+								<tr key={d.id}>
+									<td>{d.title}</td>
+									<td
+										style={{
+											textAlign: "center",
+										}}
+									>
+										{d.description}
+									</td>
+									<td className={classNames.smallTableHeader}>
+										{new Date(
+											d.dateRecorded
+										).toLocaleDateString()}
+									</td>
+									{d.resolved ? (
+										<>
+											<td
+												className={
+													classNames.smallTableHeader
+												}
+											>
+												{d.dateResolved &&
+													new Date(
+														d.dateResolved
+													).toLocaleDateString()}
+											</td>
+										</>
+									) : (
+										<td
+											className={
+												classNames.smallTableHeader
+											}
+										>
+											{"Unresolved"}
+										</td>
+									)}
+									<td className={classNames.smallTableHeader}>
+										{d.intensity}
+									</td>
+								</tr>
+							</>
+						))}
+					</tbody>
 				</table>
 			</div>
 		</>
--- Worry/src/pages/worry/Worries.tsx
+++ Worry/src/pages/worry/Worries.tsx
@@ -1,159 +1,51 @@
 import { useEffect, useState } from "react";
-import utilStyles from "../../styles/utilStyles.module.css";
+// import utilStyles from "../../styles/utilStyles.module.css";
 import { Worry } from "../../types/Worry";
 import { mockWorries } from "../../mockData/mockWorry";
-import { FontIcon, mergeStyleSets, mergeStyles } from "@fluentui/react";
-import { useDeleteWorry, useFetchWorries } from "../../hooks/WorryHooks";
-import { useNavigate } from "react-router-dom";
+// import { FontIcon, mergeStyleSets, mergeStyles } from "@fluentui/react";
+import { useFetchWorries } from "../../hooks/WorryHooks";
+// import { useNavigate } from "react-router-dom";
+import { WorryTable } from "../../pageComponents/WorryTable";
+import { TableHeader } from "../../types/TableHeader";
 
-const classNames = mergeStyleSets({
-	smallTableHeader: {
-		textAlign: "center",
-		width: "300px",
-	},
-	tableRow: {
-		height: "75px",
-		cursor: "pointer",
-	},
-});
+// const classNames = mergeStyleSets({
+// 	smallTableHeader: {
+// 		textAlign: "center",
+// 		width: "300px",
+// 	},
+// 	tableRow: {
+// 		height: "75px",
+// 		cursor: "pointer",
+// 	},
+// });
 
-const iconClass = mergeStyles({
-	fontSize: 25,
-	height: 25,
-	width: 25,
-	margin: "0 5px",
-});
+// const iconClass = mergeStyles({
+// 	fontSize: 25,
+// 	height: 25,
+// 	width: 25,
+// 	margin: "0 5px",
+// });
 
 const Worries = () => {
-	const nav = useNavigate();
+	// const nav = useNavigate();
 	const [worries, setWorries] = useState<Worry[]>([]);
 
 	const { data } = useFetchWorries();
-	const deleteWorryMutation = useDeleteWorry();
+	// const deleteWorryMutation = useDeleteWorry();
 
 	useEffect(() => {
 		setWorries(data ?? mockWorries);
 	}, [data]);
 
-	const worryHeaders = [
-		"Title",
-		"Description",
-		"Date Recorded",
-		"Date Resolved",
-		"Intensity",
-		"",
+	const worryHeaders: TableHeader[] = [
+		{ name: "Title" },
+		{ name: "Description" },
+		{ name: "Date Recorded" },
+		{ name: "Date Resolved" },
+		{ name: "Intensity" },
 	];
 
-	return (
-		<>
-			<div style={{ display: "flex", justifyContent: "center" }}>
-				<table>
-					<thead>
-						<tr>
-							<th className={utilStyles.tableHeaderWidth}>
-								Title
-							</th>
-							<th className={utilStyles.tableHeaderWidth}>
-								Description
-							</th>
-							<th className={utilStyles.tableHeaderWidth}>
-								Date Recorded
-							</th>
-							<th className={utilStyles.tableHeaderWidth}>
-								Date Resolved
-							</th>
-							<th className={utilStyles.tableHeaderWidth}>
-								Intensity
-							</th>
-							<th className={utilStyles.tableHeaderWidth}></th>
-						</tr>
-					</thead>
-					<tbody>
-						{worries.map((w) => {
-							console.log(w);
-							return (
-								<>
-									<tr
-										key={w.id}
-										className={classNames.tableRow}
-										onClick={() =>
-											nav(`/worry/edit/${w.id}`)
-										}
-									>
-										<td>{w.title}</td>
-										<td
-											style={{
-												textAlign: "center",
-											}}
-										>
-											{w.description}
-										</td>
-										<td
-											className={
-												classNames.smallTableHeader
-											}
-										>
-											{new Date(
-												w.dateRecorded
-											).toLocaleDateString()}
-										</td>
-										{w.resolved ? (
-											<>
-												<td
-													className={
-														classNames.smallTableHeader
-													}
-												>
-													{w.dateResolved &&
-														new Date(
-															w.dateResolved
-														).toLocaleDateString()}
-												</td>
-											</>
-										) : (
-											<td
-												className={
-													classNames.smallTableHeader
-												}
-											>
-												{"Unresolved"}
-											</td>
-										)}
-										<td
-											className={
-												classNames.smallTableHeader
-											}
-										>
-											{w.intensity}
-										</td>
-										<td
-											onClick={() => {
-												if (
-													window.confirm(
-														"Are you sure you want to delete this entry?"
-													)
-												) {
-													deleteWorryMutation.mutate(
-														w
-													);
-												}
-											}}
-										>
-											<FontIcon
-												aria-label="Delete"
-												iconName="Delete"
-												className={iconClass}
-											/>
-										</td>
-									</tr>
-								</>
-							);
-						})}
-					</tbody>
-				</table>
-			</div>
-		</>
-	);
+	return <WorryTable data={worries} headers={worryHeaders} />;
 };
 
 export default Worries;
