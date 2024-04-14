d8da457b1ae449cb4566e87986c002eab56994fb
bspang <bspangler21@alumni.uwosh.edu>
2024-04-13 19:11:19 -0500

    Remove startup script from repo

--- run-locally-mini.ps1
+++
@@ -1,5 +0,0 @@
-start-process powershell -WorkingDirectory "Worry" -ArgumentList '-noexit -command npm run dev'
-start-process powershell -WorkingDirectory "API" -ArgumentList '-noexit -command dotnet run'
-#start-process powershell -WorkingDirectory "GolfTrackerExpress" -ArgumentList '-noexit -command npm run dev'
-cd Worry
-code .
