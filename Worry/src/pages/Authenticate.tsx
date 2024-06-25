5a2757587209e78d6acd40be333db3f6f1c0c59e
BSPANGLER <bspangler21@alumni.uwosh.edu>
2024-06-25 15:42:04 -0500

    Create real Authenticate page

--- Worry/src/pages/Authenticate.tsx
+++ Worry/src/pages/Authenticate.tsx
@@ -1,3 +1,49 @@
+import {
+	ITheme,
+	PrimaryButton,
+	Stack,
+	TextField,
+	ThemeProvider,
+	createTheme,
+	mergeStyleSets,
+} from "@fluentui/react";
+import { fluentPalette } from "../util/fluentPalette";
+
+const fluentTheme: ITheme = createTheme({ palette: fluentPalette });
+
+const classNames = mergeStyleSets({
+	text: {
+		width: "500px",
+	},
+	section: {
+		marginBottom: "20px",
+		paddingLeft: "20px",
+	},
+});
+
 export const Authenticate = () => {
-	return <></>;
+	return (
+		<ThemeProvider theme={fluentTheme}>
+			<div className={classNames.section}>
+				<Stack>
+					<TextField
+						label="Username"
+						required
+						className={classNames.text}
+					></TextField>
+					<TextField
+						label="Password"
+						required
+						type="password"
+						canRevealPassword
+						revealPasswordAriaLabel="Show password"
+						className={classNames.text}
+					></TextField>
+				</Stack>
+			</div>
+			<div className={classNames.section}>
+				<PrimaryButton text="Login" />
+			</div>
+		</ThemeProvider>
+	);
 };
